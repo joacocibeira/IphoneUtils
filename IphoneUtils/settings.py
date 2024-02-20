1c8e6e0eac8bc9e2f02603185beab1d4f0d58dac
joacocibeira <cibeira.joaquin@gmail.com>
2024-02-20 13:14:22 -0300

    Tests for shortcuts app added

--- IphoneUtils/settings.py
+++ IphoneUtils/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from environ import Env
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,9 +86,11 @@ WSGI_APPLICATION = "IphoneUtils.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default':  env.db()
-}
+        'default':  env.db()
+    }
 
 
 # Password validation
